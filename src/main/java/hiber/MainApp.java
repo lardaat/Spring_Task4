646ad2fb79661c0471e0b175b2c23804b3704ea9
lardaat <kodymskiins@gmail.com>
2024-03-13 15:38:17 +0300

    End

--- src/main/java/hiber/MainApp.java
+++ src/main/java/hiber/MainApp.java
@@ -1,8 +1,10 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
+import hiber.service.CarService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.sql.SQLException;
@@ -14,11 +16,22 @@ public class MainApp {
             new AnnotationConfigApplicationContext(AppConfig.class);
 
       UserService userService = context.getBean(UserService.class);
+      CarService carService = context.getBean(CarService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
+      /*Car car1 = new Car("BMW", 600);
+      Car car2 = new Car("GAZ", 66);
+      carService.add(car1);
+      carService.add(car2);
+
+      userService.add(new User("User1", "Lastname1", "user1@mail.ru", car1));
+      userService.add(new User("User2", "Lastname2", "user2@mail.ru", car2));
       userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));*/
+
+//      carService.add(new Car("BMW", 600));
+//      carService.add(new Car("GAZ", 66));
+
+
 
       List<User> users = userService.listUsers();
       for (User user : users) {
@@ -26,7 +39,42 @@ public class MainApp {
          System.out.println("First Name = "+user.getFirstName());
          System.out.println("Last Name = "+user.getLastName());
          System.out.println("Email = "+user.getEmail());
+         Car userCar = user.getCar();
+         if (userCar != null) {
+            System.out.println("Car Model = " + userCar.getModel());
+            System.out.println("Car Series = " + userCar.getSeries());
+      }
          System.out.println();
+      }
+
+      List<Car> cars = carService.listCars();
+      for (Car car : cars) {
+         System.out.println("Id = "+ car.getId());
+         System.out.println("Model = "+car.getModel());
+         System.out.println("Series = "+car.getSeries());
+         System.out.println();
+      }
+
+      User userByCar = userService.getUserByCarModelAndSeries("BMW", 600);
+      if (userByCar != null) {
+         System.out.println("Пользователь, владеющий BMW 600-й серии:");
+         System.out.println("Id = " + userByCar.getId());
+         System.out.println("First Name = " + userByCar.getFirstName());
+         System.out.println("Last Name = " + userByCar.getLastName());
+         System.out.println("Email = " + userByCar.getEmail());
+      } else {
+         System.out.println("Пользователь, владеющей BMW 600-й серии не найден.");
+      }
+
+      User userByCar1 = userService.getUserByCarModelAndSeries("GAZ", 66);
+      if (userByCar1 != null) {
+         System.out.println("Пользователь, владеющий ГАЗ-66:");
+         System.out.println("Id = " + userByCar1.getId());
+         System.out.println("First Name = " + userByCar1.getFirstName());
+         System.out.println("Last Name = " + userByCar1.getLastName());
+         System.out.println("Email = " + userByCar1.getEmail());
+      } else {
+         System.out.println("Пользователь, владеющей ГАЗ-66 не найден.");
       }
 
       context.close();
--- src/main/java/hiber/dao/UserDaoImp.java
+++ src/main/java/hiber/dao/UserDaoImp.java
@@ -2,6 +2,7 @@ package hiber.dao;
 
 import hiber.model.User;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -24,6 +25,14 @@ public class UserDaoImp implements UserDao {
    public List<User> listUsers() {
       TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
       return query.getResultList();
+   }
+
+   public User getUserByCarModelAndSeries(String model, int series) {
+      String sql = "SELECT u.* FROM users u JOIN cars c ON u.car_id = c.id WHERE LOWER(c.model) = LOWER(:model) AND c.series = :series";
+      Query<User> query = sessionFactory.getCurrentSession().createNativeQuery(sql, User.class);
+      query.setParameter("model", model);
+      query.setParameter("series", series);
+      return query.uniqueResult();
    }
 
 }
